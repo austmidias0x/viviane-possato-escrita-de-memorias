070bedcc85f109bb306362f38d5c13e00f3203fc
thales <austmidias@gmail.com>
2026-09-03 18:38:29 -0300

    Otimiza carregamento da página Memórias G

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./memoriasg/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        paper: '#FDFCF8',
+        ink: '#1F1F1F',
+        subtle: '#5A5A5A',
+        accent: '#966767',
+        line: '#E6E4E0',
+        warm: '#F5F4F0',
+        rosewash: '#EFE4E2',
+      },
+      fontFamily: {
+        serif: ['"Playfair Display"', 'serif'],
+        sans: ['"Lato"', 'sans-serif'],
+        hand: ['"Caveat"', 'cursive'],
+      },
+    },
+  },
+};
